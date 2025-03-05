80d69cb4e69160696b4033545b8c26b769be90f4
devmodes <dev.clarkdave@gmail.com>
2025-03-05 13:19:36 +0800

    Added body parser

--- ip-service/src/index.ts
+++ ip-service/src/index.ts
@@ -10,6 +10,7 @@ const PORT = config("app.port");
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(router);
